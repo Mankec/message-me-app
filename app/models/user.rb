fdb031e1d62c457f5831daa81dbab8cbd3d5bcb2
milankecman270 <milan.kecman270@gmail.com>
2022-05-27 14:59:52 +0200

    lazy lazy commit

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  has_many :messages
+  has_secure_password
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 },
+  uniqueness: { case_sensitive: false }
+
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Tifa", password: "melon123")
+User.create(username: "Nick Cave", password: "melon123")
+User.create(username: "Ray Davies", password: "melon123")
+User.create(username: "Viktor Tsoi", password: "melon123")
+User.create(username: "Joakim Broden", password: "melon123")
+
+Message.create(body: "Hi I am Alen Islamovic", user: User.find(2) )
+Message.create(body: "Hi I am Nick Cave", user: User.find(3) )
+Message.create(body: "Hi I am Ray Davies", user: User.find(4) )
+Message.create(body: "Hi I am Viktor Tsoi", user: User.find(5) )
+Message.create(body: "Hi I am Joakim Broden", user: User.find(6) )
